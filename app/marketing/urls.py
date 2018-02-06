b543708afbcf17360fc5fb566929fa089a1e329a
Jason Haas <jasonrhaas@gmail.com>
2018-02-05 18:44:48 -0600

    MVP is complete

--- app/marketing/urls.py
+++ app/marketing/urls.py
@@ -5,14 +5,14 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
-    path('get_email/', views.get_email, name='get_email'),
+    path('submit/', views.submit, name='submit'),
     path('thanks/', views.thanks, name='thanks'),
     path('pricing/', views.pricing, name='pricing'),
     path('cart/', views.cart, name='cart'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    # path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- app/marketing/views.py
+++ app/marketing/views.py
@@ -6,44 +6,36 @@ from .models import Question, InterestedPerson
 from .forms import EmailForm
 from django.core.mail import send_mail
 
-# send_mail(
-#     'Subject here',
-#     'Here is the message.',
-#     'from@example.com',
-#     ['to@example.com'],
-#     fail_silently=False,
-# )
 
+def submit(request):
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = EmailForm()
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {'latest_question_list': latest_question_list}
-#     return HttpResponse(template.render(context, request))
+    context = dict(form=form)
+
+    return render(request, 'marketing/form.html', context)
 
 
 def index(request):
-    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
         form = EmailForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
             form.save()
             return HttpResponseRedirect('/thanks/')
 
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = EmailForm()
 
     heading_0 = dict(title='AeroPress XL',
-                     blurb="""Use the AeroPress with Wide Mouth Mugs"""
+                     blurb="""An AeroPress Adapter to use with Wide Mouth Mugs"""
                      )
     heading_1 = dict(title='Fits perfectly on large size mugs',
-                     blurb="""Examples:  Github Mug, Life is Good Mug, etc."""
+                     blurb="""Examples:  Github Mug, Starbucks Mug, and more."""
                      )
     heading_2 = dict(title='Simple Integration',
                      blurb="""Just place on top of your mug, and use the AeroPress as normal."""
@@ -55,7 +47,8 @@ def index(request):
 
     context = dict(company=company,
                    heading=[heading_0, heading_1, heading_2],
-                   form=form)
+                   form=form,
+                   )
 
     # print(form)
 
@@ -68,46 +61,30 @@ def thanks(request):
 
 
 def pricing(request):
-    context = {}
+    if request.method == 'POST':
+        print(request.POST)
+        form = EmailForm(request.POST)
+        print(form)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = EmailForm()
+
+    context = dict(form=form)
+
     return render(request, 'marketing/pricing.html', context)
 
 
 def cart(request):
-    context = {}
-    return render(request, 'marketing/cart.html', context)
-
-
-def get_email(request):
-    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
         form = EmailForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
+            form.save()
             return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = EmailForm()
 
-    context = {'form': form}
-    print(context)
+    context = dict(form=form)
 
-    return render(request, 'marketing/form.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'marketing/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return render(request, 'marketing/cart.html', context)
