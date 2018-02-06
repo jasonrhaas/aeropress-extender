a9e2dc96335b5b1845eabe85cde2947ac587247c
Jason Haas <jasonrhaas@gmail.com>
2018-02-06 09:10:17 -0600

    Redirect to SSL

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECURE_SSL_REDIRECT = True
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
