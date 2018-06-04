b36b3404ee3db7c67ef53b7ab902743923cb6ffe
Jason Haas <jasonrhaas@gmail.com>
2018-06-04 13:39:13 -0500

    enable SSL redirect

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 
 
 # Quick-start development settings - unsuitable for production
